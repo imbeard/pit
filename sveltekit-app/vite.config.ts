e946a6e155e0fb05f373bbf9c5bfef313cbb18d0
Rocco Punghellini <rocco.punghellini@gmail.com>
2025-06-16 18:40:43 +0200

    add devjson

--- sveltekit-app/vite.config.ts
+++ sveltekit-app/vite.config.ts
@@ -2,7 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { vite as vidstack } from 'vidstack/plugins';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
+import devtoolsJson from "vite-plugin-devtools-json";
+
 
 export default defineConfig({
-	plugins: [vidstack(), sveltekit(), tailwindcss()]
+	plugins: [vidstack(), sveltekit(), tailwindcss(), devtoolsJson()],
 });
