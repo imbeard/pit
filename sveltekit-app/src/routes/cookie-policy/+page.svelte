b0965ea652eb61120699fb0fafa432324918964f
Rocco Punghellini <rocco.punghellini@gmail.com>
2025-11-13 09:48:14 +0100

    cookie policy

--- sveltekit-app/src/routes/cookie-policy/+page.svelte
+++ sveltekit-app/src/routes/cookie-policy/+page.svelte
@@ -1,10 +1,10 @@
 <script>
 	import PortableText from '$lib/components/element/PortableText.svelte';
 	export let data;
-	$: policy = data?.page?.data?.privacyPolicy;
+	$: policy = data?.page?.data?.cookiePolicy;
 </script>
 
 <div class="flex flex-col justify-start pt-20 items-center md:justify-center min-h-screen px-xs">
 	<h1 class="typo-base">Cookie Policy</h1>
-    <div class="max-w-6xl pt-3"><PortableText data={policy} /></div>
+	<div class="max-w-6xl pt-3"><PortableText data={policy} /></div>
 </div>
